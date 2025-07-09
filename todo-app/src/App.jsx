86f79bda374e250aeb77bccbfa1395bbbfdd09ac
BingWei <bingwei1975@@gmail.com>
2025-07-10 01:55:52 +0800

    header & tabs added

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,19 +1,16 @@
-import './App.css'
 import { Header } from './components/Header'
 import { Tabs } from './components/Tabs'
 import { TodoInput } from './components/ToDoInput'
 import { TodoList } from './components/ToDoList'
 
 function App() {
-  
-
   return (
-    <div>
+    <>
       <Header />
       <Tabs />
       <TodoList />
       <TodoInput />
-    </div>
+    </>
   )
 }
 
